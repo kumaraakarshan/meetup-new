1933e24c62a282eb8262fc19c3a1a6cbd7994c20
kumaraakarshan <aakarshan0005@gmail.com>
2023-10-08 19:07:12 +0530

    get and post request

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d8be87de0a5f2173"; }
+/******/ 		__webpack_require__.h = function() { return "8312f0e14a416981"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
