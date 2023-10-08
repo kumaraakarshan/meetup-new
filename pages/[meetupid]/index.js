3394b43a25edf1411b7a245ac9b37270e2b4b3dd
kumaraakarshan <aakarshan0005@gmail.com>
2023-10-08 15:53:02 +0530

    first commit

---
+++ pages/[meetupid]/index.js
@@ -0,0 +1,8 @@
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/Meetupdetail";
+function MeetupDetails(props) {
+    return <MeetupDetail image ='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg' title='first meetup address =SomeStreet some city' description='this is a meetup ' />
+   
+}
+
+export default MeetupDetails
